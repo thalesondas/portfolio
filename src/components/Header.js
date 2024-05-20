81b85773d2c9b8a1c88d95e080da47c749b99d11
Thales <thalesondas0@gmail.com>
2024-05-20 19:57:45 -0300

    Colocando React-Bootstrap e fazendo Header/Nav básico

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
-import '../assets/Header.css'
+import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
+import '../assets/Header.css';
 
 const Header = () => {
-    return(
+    return (
         <header>
-            <h1>Título Header</h1>
+            <Container className='py-3'>
+                <Row className='d-flex align-items-center'>
+                    <Col>
+                        <h1>Imagem de alguma coisa</h1>
+                    </Col>
+                    <Col className='d-flex align-items-center justify-content-end'>
+                        <Navbar>
+                            <Nav>
+                                <Nav.Link href="#">Algo 1</Nav.Link>
+                                <Nav.Link href="#">Algo 2</Nav.Link>
+                                <Nav.Link href="#">Algo 3</Nav.Link>
+                            </Nav>
+                        </Navbar>
+                    </Col>
+                </Row>
+            </Container>
         </header>
-    )
+    );
 }
 
 export default Header
