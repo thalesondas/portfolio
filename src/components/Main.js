c4eaafc024c1f7f53f0425ae4ef90723d41bf62c
Thales <thalesondas0@gmail.com>
2024-05-22 10:30:37 -0300

    Começando a fazer Main e colocando animação na logo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,25 @@
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import '../assets/Main.css'
+import ProgrammerLogo from '../images/ProgrammerLogo.png';
 
 const Main = () => {
     return(
         <main>
-            <h2>Título Main</h2>
+            <Container style={{paddingTop: '100px'}}>
+                <Row style={{ height: '629px'}}>
+                    <Col className='main-home-col1 d-flex justify-content-center align-items-center'>
+                        <h1>Olá, eu sou<br />
+                            <span>Thales Ondas </span><span>|</span><br />
+                            <br />
+                            Desenvolvedor Full Stack<br />
+                            Analista de Dados
+                        </h1>
+                    </Col>
+                    <Col className='main-home-col2 d-flex justify-content-center align-items-center'>
+                        <Image src={ProgrammerLogo}></Image>
+                    </Col>
+                </Row>
+            </Container>
         </main>
     )
 }
