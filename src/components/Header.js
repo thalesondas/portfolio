7fa9c68954c4f0749873a8975549d1371e65410c
Thales <thalesondas0@gmail.com>
2024-06-02 18:49:44 -0300

    Deixando <Header> responsivo

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,24 +7,27 @@ const Header = () => {
         <header>
             <Container className='py-2 px-5'>
                 <Row className='d-flex align-items-center'>
-                    <Col className='d-flex align-items-start'>
+                    <Col sm={7} className='d-flex align-items-start'>
                         <Row>
                             <Col className='d-flex justify-content-end'>
-                                <Image src={ProgrammerLogo} style={{ height: '60px' }}></Image>
+                                <Image className='img-header' src={ProgrammerLogo}></Image>
                             </Col>
                             <Col className='d-flex align-items-end'>
                                 <h1 className='cormorant'>Thales.dev</h1>
                             </Col>
                         </Row>
                     </Col>
-                    <Col className='d-flex align-items-center justify-content-end'>
-                        <Navbar>
-                            <Nav>
-                                <Nav.Link className='me-4' href="#home">Home</Nav.Link>
-                                <Nav.Link className='mx-4' href="#sobre">Sobre mim</Nav.Link>
-                                <Nav.Link className='mx-4' href="#habilidades">Habilidades</Nav.Link>
-                                <Nav.Link className='ms-4' href="#projetos">Projetos</Nav.Link>
+                    <Col sm={5} className='d-flex align-items-center justify-content-end'>
+                        <Navbar expand="md">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                        <Navbar.Collapse className="justify-content-end">
+                            <Nav className='ms-auto nav-gap'>
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#sobre">Sobre mim</Nav.Link>
+                                <Nav.Link href="#habilidades">Habilidades</Nav.Link>
+                                <Nav.Link href="#projetos">Projetos</Nav.Link>
                             </Nav>
+                        </Navbar.Collapse>
                         </Navbar>
                     </Col>
                 </Row>
