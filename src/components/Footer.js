4ab3a00004a84125911e2d462ec5f61fb4621a05
Thales <thalesondas0@gmail.com>
2024-07-03 17:23:59 -0300

    Colocando ícone de email no Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
-import { Col } from "react-bootstrap";
+import { Col, Image } from "react-bootstrap";
 import RedesSociaisFooter from "./RedesSociaisFooter";
+import Email from '../images/email.svg'
 import '../assets/Footer.css'
 
 const Footer = () => {
     return(
         <footer className="d-flex justify-content-center align-items-center">
             <Col className="d-flex justify-content-center align-items-center">
-                <p>E-mail para contato: <span className="span-email">thalesondas0@gmail.com</span></p>
+                <p><Image className='img-email pe-2' src={Email} /> <span className="span-email">thalesondas0@gmail.com</span></p>
             </Col>
             <Col className="d-flex justify-content-center align-items-center">
                 <RedesSociaisFooter texto='LinkedIn'/>
