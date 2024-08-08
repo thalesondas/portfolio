45758f4b1a5314bc2f7ea7e72a6af2c8f1a1a6b7
Thales <thalesondas0@gmail.com>
2024-08-08 17:38:47 -0300

    Mudando a título do meu portfólio

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Provider } from 'react-redux'
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,6 +12,11 @@ import ParticlesComponent from './particles';
 import './App.css';
 
 const App = () => {
+
+  useEffect(() => {
+    document.title = 'Meu Portfólio - Thales Ondas';
+  }, []);
+
   return (
     <>
       <Provider store={store}>
