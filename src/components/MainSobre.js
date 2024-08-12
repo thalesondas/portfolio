9b6e8b5392f846150b7b4150d9ea584bf094acbe
Thales <thalesondas0@gmail.com>
2024-08-12 14:57:55 -0300

    Melhorando a responsividade do Main-Sobre em dispositivos medium para cima

--- src/components/MainSobre.js
+++ src/components/MainSobre.js
@@ -14,16 +14,16 @@ const MainSobre = () => {
     }, [])
 
     return(
-        <Container id="sobre" className="main-sobre d-flex">
+        <Container id="sobre" className="main-sobre d-flex flex-column-reverse">
             <Row>
-                <Col data-aos="fade-right" data-aos-duration="1500" md={5} className="d-flex flex-column justify-content-center align-items-center">
+                <Col data-aos="fade-right" data-aos-duration="1500" md={6} lg={5} className="d-flex flex-column justify-content-center align-items-start align-items-xl-center">
                     <TiltComponent />
-                    <p className="p-email mt-5"> <Image className='img-email pe-2' src={Email} /> <span className="span-email">thalesondas0@gmail.com</span></p>
+                    <p className="mt-5 ms-3 ms-lg-5"> <Image className='img-email pe-2' src={Email} /> <span className="span-email">thalesondas0@gmail.com</span></p>
                 </Col>
-                <Col data-aos="fade-left" data-aos-duration="1500" md={7} className="main-sobre-col2 d-flex flex-column justify-content-center">
+                <Col data-aos="fade-left" data-aos-duration="1500" md={6} lg={7} className="main-sobre-col2 d-flex flex-column justify-content-center">
                     <h2 data-aos="fade-down" data-aos-duration="1700" className="mb-4"><span className="barra-vermelha">|</span> Sobre mim</h2>
                     <p>
-                        &emsp;Olá! Sou formado em Análise e Desenvolvimento de Sistemas pela UTFPR de Cornélio Procópio,
+                        &emsp;Olá! Estou me formando em Análise e Desenvolvimento de Sistemas pela UTFPR de Cornélio Procópio,
                         onde adquiri conhecimento em várias linguagens de programação,
                         linguagens de marcação e estilização, bancos de dados, bibliotecas e frameworks, etc.
                     </p>
